639818a29cf83163482a865c70bfe498776a16e4
AhmedIsmeil206 <120604148+AhmedIsmeil206@users.noreply.github.com>
2024-09-05 22:59:34 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
+import HomePage from './pages/HomePage';
+import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
+import ProductPage from './pages/ProductPage';
+import Header from './components/Header/Header';
+import { ProductProvider } from './context/ProductContext';
+import { CartProvider } from './context/CartContext';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ProductProvider>
+      <CartProvider>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/product/:productId" element={<ProductPage />} />
+          </Routes>
+        </Router>
+      </CartProvider>
+    </ProductProvider>
   );
-}
+};
 
 export default App;
